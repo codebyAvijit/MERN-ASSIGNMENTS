c48082f0f22f03ac08a79a320344ec17f68c8de3
Avijit Pateriya <deviljit.ap@gmail.com>
2023-04-09 15:29:17 +0530

    pattern

--- codesAsked/pattern.js
+++ codesAsked/pattern.js
@@ -1,18 +1,19 @@
 function patternPrinting(rows) {
-  for (let i = 1; i <= rows; i++) {
-    let row = "";
-    for (let j = i; j <= rows; j++) {
-      if (i == 1) {
-        row += "*";
-      } else if (j == i) {
-        row += "*";
-      } else if (j == rows) {
-        row += "*";
+  //outer loop works as rows
+  //inner loop works as columns
+  for (i = 0; i < 6; i++) {
+    for (let j = 0; j < rows - i; j++) {
+      if (i == 0 || i == 5) {
+        process.stdout.write("*");
       } else {
-        row += " ";
+        if (j == 0 || j == rows - i - 1) {
+          process.stdout.write("*");
+        } else {
+          process.stdout.write(" ");
+        }
       }
     }
-    console.log(row);
+    console.log("");
   }
 }
 let numRows = 6;
