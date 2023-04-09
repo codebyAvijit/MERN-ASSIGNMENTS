9f4ab4da2f7167ad7f9417c94b1e5576e01c7452
Avijit Pateriya <deviljit.ap@gmail.com>
2023-04-09 15:30:47 +0530

    code change

--- codesAsked/pattern.js
+++ codesAsked/pattern.js
@@ -1,9 +1,9 @@
 function patternPrinting(rows) {
   //outer loop works as rows
   //inner loop works as columns
-  for (i = 0; i < 6; i++) {
+  for (i = 0; i < rows; i++) {
     for (let j = 0; j < rows - i; j++) {
-      if (i == 0 || i == 5) {
+      if (i == 0 || i == rows - 1) {
         process.stdout.write("*");
       } else {
         if (j == 0 || j == rows - i - 1) {
